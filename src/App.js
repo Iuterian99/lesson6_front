f90a2ca78c0ae321ab495d85439341ea230cef4e
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-30 18:21:26 +0500

    half

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+const Airports = gql`
+  query {
+    airports {
+      airport_id
+      airport_name
+      airport_location
+    }
+  }
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { data, loading, error } = useQuery(Airports);
+  console.log(data);
+
+  return <>salom</>;
 }
 
 export default App;
