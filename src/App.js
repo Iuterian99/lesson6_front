e8430aefc4897a37d7a372236f3a39a1161b0e4f
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-31 22:31:28 +0500

    goood

--- src/App.js
+++ src/App.js
@@ -9,11 +9,28 @@ const Airports = gql`
     }
   }
 `;
+
+const Airport_reyses = gql`
+  query airport_reyses($airport_id: ID!) {
+    airport_reyses(airport_id: $airport_id) {
+      reyse_to
+    }
+  }
+`;
 function App() {
   const { data, loading, error } = useQuery(Airports);
-  console.log(data);
+  const { data: reysData } = useQuery(Airport_reyses, {
+    variables: { airport_id: "d64b800d-d3a6-4e01-8cd1-4d576ac7118e" },
+  });
+  console.log(reysData);
 
-  return <>salom</>;
+  return (
+    <>
+      {loading && <>loading</>}
+      {error && <>error</>}
+      {data && data.airports.map((e, i) => <h3 key={i}>{e.airport_name}</h3>)}
+    </>
+  );
 }
 
 export default App;
